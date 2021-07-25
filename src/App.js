0f5adb7e40432fd8f7c91b1326c311c1117c42d5
isaiahcedano <isaiahcedano14@gmail.com>
2021-07-25 05:40:28 -0500

    Instalado los dependencias del front-end de Jerson

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import './App.css';
 import Navigation from './components/Navigation'
 import NotesList from './components/NoteList'
 import CreateNote from './components/CreateNote'
@@ -34,9 +33,6 @@ function App() {
           <div class="col-md-5"><h3 class="card-title tittle-register">Text-To-Speech</h3></div>
           <div class="col-md-1"><p></p> </div>
         </div>
-
-
-
       </div> */}
 
 
@@ -57,7 +53,6 @@ function App() {
               </thead>
 
               <tr>
-
                 <th scope="col" >
                   <input type="text"
                          className="form-control" placeholder="Url">
