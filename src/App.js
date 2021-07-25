e3ea153542386ae2d1fe3d06bdcb34acef84174b
isaiahcedano <isaiahcedano14@gmail.com>
2021-07-25 05:31:25 -0500

    Agregado la parte front-end de Jerson

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+import Navigation from './components/Navigation'
+import NotesList from './components/NoteList'
+import CreateNote from './components/CreateNote'
+import CreateUser from './components/CreateUser'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Navigation />
+        {/* <div class>
+        <div class="row" >
+          <div class="col-md-1"><br></br></div>
+          <div class="col-md-10" ><br></br></div>
+          <div class="col-md-1"  ><br></br></div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-1"><p> </p></div>
+          <div class="col-md-3"><h2 class="card-title tittle-register">Aqui va el logo</h2></div>
+          <div class="col-md-7"><h2 class="card-title tittle-register">Bienvenido</h2></div>
+          <div class="col-md-1"><p> </p></div>
+        </div>
+        <div class="row" >
+          <div class="col-md-1"><br></br></div>
+          <div class="col-md-10" ><br></br></div>
+          <div class="col-md-1"  ><br></br></div>
+        </div>
+        <div class="row" >
+          <div class="col-md-1"><p></p></div>
+          <div class="col-md-5"><h3 class="card-title tittle-register">Detector de Rostros</h3></div>
+          <div class="col-md-5"><h3 class="card-title tittle-register">Text-To-Speech</h3></div>
+          <div class="col-md-1"><p></p> </div>
+        </div>
+
+
+
+      </div> */}
+
+
+        <div class="page-item container-fluid container_exp">
+          <div class="container my-4">
+            <div class="text-center">
+              <h3 class="card-title tittle-register">Bienvenido</h3>
+            </div>
+          </div>
+
+          <div class="card">
+            <table class="table table-hover text-center">
+              <thead>
+              <tr>
+                <th scope="col">Detector de rostros</th>
+                <th scope="col">Text-to-Speech</th>
+              </tr>
+              </thead>
+
+              <tr>
+
+                <th scope="col" >
+                  <input type="text"
+                         className="form-control" placeholder="Url">
+                  </input>
+                </th>
+                <th scope="col">
+                  <input type="text"
+                         className="form-control" placeholder="Ingrese texto">
+                  </input>
+                </th>
+              </tr>
+              <br></br>
+              <tr>
+                <th>
+                  <button type="button" class="btn btn-secondary">Detectar</button>
+                </th>
+                <button type="button" class="btn btn-secondary">Convertir</button>
+                <br></br>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </Router>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class Navigation extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">
+                        Principal
+                        </Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="container">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <Link className="nav-link" to="/">Notes</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link className="nav-link" to="/create">CreateNote</Link>
+                            </li>
+                            <li clas="nav-item">
+                                <Link className="nav-link" to="/User">CreateUser</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+
+            </nav>
+        )
+    }
+}
